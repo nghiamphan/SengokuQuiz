8fe587bd5ed9d5f66f9f3c71e2976de2e1b94fd7
nghiamphan <nphan@fandm.edu>
2018-05-07 16:03:19 -0700

    saved in bundle, second activity, intent

--- app/src/main/java/com/nphan/android/sengokuquiz/SengokuActivity.java
+++ app/src/main/java/com/nphan/android/sengokuquiz/SengokuActivity.java
@@ -1,8 +1,8 @@
 package com.nphan.android.sengokuquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,25 +10,42 @@ import android.widget.Toast;
 
 public class SengokuActivity extends AppCompatActivity {
 
+    private static final String KEY_INDEX = "index";
+    private static final String ALREADY_ANSWERED = "already_answered";
+    private static final String CORRECT_ANSWERED = "number of correct answers";
+    private static final String TOTAL_ANSWERED = "number of questions answered";
+
     private TextView mQuestionTextView;
     private Button mOptionA_Button;
     private Button mOptionB_Button;
     private Button mOptionC_Button;
     private Button mOptionD_Button;
     private Button mNextButton;
+    private Button mFinishButton;
+    private TextView mNumberCorrectAnswersTextView;
     
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_1, R.string.question_1_A, R.string.question_1_B, R.string.question_1_C, R.string.question_1_D, R.string.question_1_answer),
             new Question(R.string.question_2, R.string.question_2_A, R.string.question_2_B, R.string.question_2_C, R.string.question_2_D, R.string.question_2_answer)
     };
 
+    private boolean[] mAlreadyAnswered = new boolean[mQuestionBank.length]; // default value = false
+
     private int mCurrentIndex = 0;
+    private int mNumberCorrectAnswers = 0;
+    private int mTotalAnswered = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sengoku);
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mAlreadyAnswered = savedInstanceState.getBooleanArray(ALREADY_ANSWERED);
+            mNumberCorrectAnswers = savedInstanceState.getInt(CORRECT_ANSWERED, 0);
+            mTotalAnswered = savedInstanceState.getInt(TOTAL_ANSWERED, 0);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view);
 
@@ -75,7 +92,27 @@ public class SengokuActivity extends AppCompatActivity {
             }
         });
 
+        mNumberCorrectAnswersTextView = findViewById(R.id.number_correct_answers_text_view);
+
+        mFinishButton = findViewById(R.id.finish_button_view);
+        mFinishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = SummaryActivity.newIntent(SengokuActivity.this, mNumberCorrectAnswers, mTotalAnswered);
+                startActivity(intent);
+            }
+        });
+
         updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBooleanArray(ALREADY_ANSWERED, mAlreadyAnswered);
+        savedInstanceState.putInt(CORRECT_ANSWERED, mNumberCorrectAnswers);
+        savedInstanceState.putInt(TOTAL_ANSWERED, mTotalAnswered);
     }
 
     private void updateQuestion() {
@@ -85,6 +122,8 @@ public class SengokuActivity extends AppCompatActivity {
         mOptionB_Button.setText(question.getOptionBId());
         mOptionC_Button.setText(question.getOptionCId());
         mOptionD_Button.setText(question.getOptionDId());
+        updateButtons();
+        updateScore();
     }
 
     private void checkAnswer(int userAnswer) {
@@ -94,9 +133,37 @@ public class SengokuActivity extends AppCompatActivity {
         int toastMessageId = R.string.incorrect_toast;
         if (userAnswer == Integer.valueOf(answer)) {
             toastMessageId = R.string.correct_toast;
+            mNumberCorrectAnswers += 1;
         }
+        mTotalAnswered += 1;
 
         Toast.makeText(this, toastMessageId, Toast.LENGTH_SHORT)
                 .show();
+
+        mAlreadyAnswered[mCurrentIndex] = true;
+        updateButtons();
+        updateScore();
+    }
+
+    private void updateButtons() {
+        if (mAlreadyAnswered[mCurrentIndex]) {
+            mOptionA_Button.setEnabled(false);
+            mOptionB_Button.setEnabled(false);
+            mOptionC_Button.setEnabled(false);
+            mOptionD_Button.setEnabled(false);
+            mNextButton.setEnabled(true);
+        }
+        else {
+            mOptionA_Button.setEnabled(true);
+            mOptionB_Button.setEnabled(true);
+            mOptionC_Button.setEnabled(true);
+            mOptionD_Button.setEnabled(true);
+            mNextButton.setEnabled(false);
+        }
+    }
+
+    private void updateScore() {
+        String correctAnswersText = getResources().getString(R.string.number_correct_answers);
+        mNumberCorrectAnswersTextView.setText(correctAnswersText + " " + mNumberCorrectAnswers); // Later, use resource with placeholders
     }
 }
---
+++ app/src/main/java/com/nphan/android/sengokuquiz/SummaryActivity.java
@@ -0,0 +1,54 @@
+package com.nphan.android.sengokuquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CORRECT_ANSWERS = "com.nphan.android.sengokuquiz.number_correct_answers";
+    private static final String EXTRA_TOTAL_ANSWERED = "com.nphan.android.sengokuquiz.total_questions_answered";
+
+    private TextView mNumberCorrectAnswersTextView;
+    private TextView mTotalAnsweredTextView;
+    private TextView mScoreTextView;
+
+    private int mNumberCorrectAnswers;
+    private int mTotalAnswered;
+
+    public static Intent newIntent(Context context, int numberCorrectAnswers, int totalAnswered) {
+        Intent intent = new Intent(context, SummaryActivity.class);
+        intent.putExtra(EXTRA_CORRECT_ANSWERS, numberCorrectAnswers);
+        intent.putExtra(EXTRA_TOTAL_ANSWERED, totalAnswered);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+        mNumberCorrectAnswers = getIntent().getIntExtra(EXTRA_CORRECT_ANSWERS, 0);
+        mTotalAnswered = getIntent().getIntExtra(EXTRA_TOTAL_ANSWERED, 0);
+
+        mNumberCorrectAnswersTextView = findViewById(R.id.number_correct_answers_text_view);
+        String correctAnswersText = getResources().getString(R.string.number_correct_answers);
+        mNumberCorrectAnswersTextView.setText(correctAnswersText + " " + mNumberCorrectAnswers);
+
+        mTotalAnsweredTextView = findViewById(R.id.total_answers_text_view);
+        String totalAnsweredText = getResources().getString(R.string.total_answers);
+        mTotalAnsweredTextView.setText(totalAnsweredText + " " + mTotalAnswered);
+
+        mScoreTextView = findViewById(R.id.score_text_view);
+        float score = 0;
+        if (mTotalAnswered != 0) {
+            score = (float) mNumberCorrectAnswers / mTotalAnswered * 100;
+        }
+        String scoreText = getResources().getString(R.string.score);
+        mScoreTextView.setText(scoreText + " " + String.format("%.2f", score) + "%");
+    }
+}
